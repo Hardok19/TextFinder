807186d80cc84866175ba7b9f665a24b332b263f
Dylan <dyelizondo@estudiantec.cr>
2024-05-03 17:30:37 -0600

    Añade soporte para la lectura de archivos PDF y DOCX
    Este commit introduce dos nuevas clases, PDFFileReader y DocxFileReader, que extienden la funcionalidad de nuestra aplicación para manejar la lectura de archivos PDF y DOCX, respectivamente. Ahora, la aplicación puede extraer texto de estos tipos de documentos y procesarlo para su inserción en el árbol AVL, facilitando la gestión de palabras y su búsqueda posterior.
    
    Cambios realizados:
    - Creación de PDFFileReader para procesar archivos PDF utilizando Apache PDFBox.
    - Creación de DocxFileReader para procesar archivos DOCX utilizando Apache POI.
    - Modificación de la clase Main para demostrar la funcionalidad de lectura de ambos tipos de archivos.
    - Actualización de las dependencias en pom.xml para incluir las bibliotecas necesarias de Apache PDFBox y Apache POI.
    
    Estos cambios permiten que la aplicación maneje una variedad más amplia de formatos de documentos, mejorando su utilidad y flexibilidad.

--- src/main/java/org/finder/DocxFileReader.java
+++ src/main/java/org/finder/DocxFileReader.java
@@ -8,14 +8,30 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Clase para leer archivos DOCX y extraer palabras para su inserción en un árbol AVL.
+ * Esta clase utiliza la biblioteca Apache POI para procesar archivos DOCX.
+ * Extrae todas las palabras de cada párrafo del documento y las inserta en el árbol AVL,
+ * manteniendo un conteo de su posición para facilitar búsquedas y referencias futuras.
+ */
 public class DocxFileReader {
     private static final Logger logger = LogManager.getLogger(DocxFileReader.class);
-    private AVLTree avlTree;
-
+    private AVLTree avlTree;// Árbol AVL donde se insertarán las palabras.
+    /**
+     * Constructor que inicializa el DocxFileReader con un árbol AVL específico.
+     *
+     * @param avlTree el árbol AVL en el que se insertarán las palabras.
+     */
     public DocxFileReader(AVLTree avlTree) {
         this.avlTree = avlTree;
     }
 
+    /**
+     * Lee un archivo DOCX desde una ruta de archivo especificada y extrae palabras de cada párrafo.
+     * Las palabras extraídas se insertan en el árbol AVL junto con su posición en el texto.
+     *
+     * @param filePath la ruta del sistema de archivos hacia el archivo DOCX que se va a procesar.
+     */
     public void readFileAndInsertWords(String filePath) {
         try (FileInputStream fis = new FileInputStream(filePath);
              XWPFDocument document = new XWPFDocument(fis)) {
